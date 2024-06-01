19fbf97db063dbc89af27f1a81345bdd7a16e5e6
Gui-gt <30755guilherme@gmail.com>
2024-06-01 19:14:32 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,3 +32,59 @@ namespace Program
         }
     }
 }
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int horas = 7;
+
+            while(horas >= 7 && horas <= 17){
+                
+                Console.WriteLine("Horas : "+ horas + " mercado aberto!");
+                horas++;
+            }
+            Console.WriteLine("Mercado fechou pós 17");
+        }
+    }
+}
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] Pizzaria = {"Calabreza","Portuguesa","Salmão","Italiana","Frango Catupiry"};
+
+            foreach (var pizza in Pizzaria)
+            {
+                Console.WriteLine("Temos o sabor: "+ pizza+" bom apetite!");
+            }
+        }
+    }
+}
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Contagem regressiva para o lançamento do foguete!");
+            for(int c = 1000;c > 0;c--){
+                
+                Console.WriteLine("Contando "+c);
+                
+            }
+
+            Console.WriteLine("Foguete em lançamento");
+        }
+    }
+}
