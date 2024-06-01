781991ae703f74930802d11e6b2f287b92c5433f
Gui-gt <30755guilherme@gmail.com>
2024-06-01 18:39:34 -0300

    astt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿using System;
-
-namespace Praticando
+﻿
+namespace PRATICANDO_C_
 {
-    
+    public class Program
+    {
+        static void Main ()
+        {
+            int recebe_area = Praça(250,350);
+            Console.WriteLine("o tamanho da praça é: " + recebe_area + " m2");
+        }
+
+        static int Praça(int Base, int Altura)
+        {
+            int area = (Base * Altura) \2;
+            return area;
+
+        }
+    }
 }
